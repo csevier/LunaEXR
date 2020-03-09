a4123d5c4977b49b2c8e4bb5f7a2c1e3b7da30f3
Cody Sevier <sevierit@gmail.com>
2020-03-08 20:56:12 -0500

    fixed linking issue, everything now builds.

---
+++ Luna/luna_math/src/geometry.cpp
@@ -0,0 +1,45 @@
+#include "geometry.hpp"
+
+namespace luna
+{
+	float CircleCircumference(float radius)
+	{
+		return 2 * cPI * radius;
+	}
+
+	float AreaOfCircle(float radius)
+	{
+		return cPI * std::pow(radius, 2);
+	}
+
+	float AreaOfRectangle(float length, float width)
+	{
+		return length * width;
+	}
+
+
+	float Sec(Angle angle)
+	{
+		return 1 / std::cosf(angle.Radians());
+	}
+
+	float Csc(Angle angle)
+	{
+		return 1 / std::sinf(angle.Radians());
+	}
+
+	float Cot(Angle angle)
+	{
+		return 1 / std::tanf(angle.Radians());
+	}
+
+	float ArcLengthRadians(float radius, Angle angle)
+	{
+		return angle.Radians() * radius;
+	}
+
+	float ArcLengthDegrees(float radius, Angle angle)
+	{
+		return (angle.Degrees() / 360) * 2 * cPI * radius;
+	}
+}
--- Luna/luna_math/src/geometry.hpp
+++ Luna/luna_math/src/geometry.hpp
@@ -5,45 +5,13 @@
 
 namespace luna
 {
-	float CircleCircumference(float radius)
-	{
-		return 2 * cPI * radius;
-	}
-
-	float AreaOfCircle(float radius)
-	{
-		return cPI * std::pow(radius, 2);
-	}
-
-	float AreaOfRectangle(float length, float width)
-	{
-		return length * width;
-	}
-
-
-	float Sec(Angle angle)
-	{
-		return 1 / std::cosf(angle.Radians());
-	}
-
-	float Csc(Angle angle)
-	{
-		return 1 / std::sinf(angle.Radians());
-	}
-
-	float Cot(Angle angle)
-	{
-		return 1 / std::tanf(angle.Radians());
-	}
-
-	float ArcLengthRadians(float radius, Angle angle)
-	{
-		return angle.Radians() * radius;
-	}
-
-	float ArcLengthDegrees(float radius, Angle angle)
-	{
-		return (angle.Degrees() / 360) * 2 * cPI * radius;
-	}
+	float CircleCircumference(float radius);
+	float AreaOfCircle(float radius);
+	float AreaOfRectangle(float length, float width);
+	float Sec(Angle angle);
+	float Csc(Angle angle);
+	float Cot(Angle angle);
+	float ArcLengthRadians(float radius, Angle angle);
+	float ArcLengthDegrees(float radius, Angle angle);
 }
 #endif
